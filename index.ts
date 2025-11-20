b2718213eb1086e2692a6db7bc560c1f158ce6d9
Edun Yusuf <yusufedun19@gmail.com>
2025-11-20 12:50:55 +0100

    latest

--- index.ts
+++ index.ts
@@ -5,7 +5,16 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200)
+    } else {
+        next()
+    }
+})
 app.get('/', (req, res) => {
     res.send('Hello from Express + TSX')
 })
